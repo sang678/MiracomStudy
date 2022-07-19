a33113458b7fb2ccbcb2f4d0b140fd6b000daaf0
DESKTOP-A0OC3DD\CrazyCow <tkdrmsdlzz@naver.com>
2022-07-19 20:07:23 +0900

    Base

---
+++ src/main/java/com/cloud/study/CloudStudyApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/study/CloudStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
